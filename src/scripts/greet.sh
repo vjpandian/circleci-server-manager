5ad4389e4176dbe918ee5e314b690171fcd0f38e
Vijay Pandian <ananth.vijay@gmail.com>
2024-03-04 17:18:03 -0500

    push changes

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo << parameters.agent_version >>
-echo << parameters.helm_version >>
+echo "<< parameters.agent_version >>"
+echo "<< parameters.helm_version >>"
 
 #helm registry login cciserver.azurecr.io -u $AZURECR_USERNAME -p $AZURECR_PASSWORD
 ## Fetch the Helm chart for inspection. Replace `<version>` with the full version of CircleCI server.
